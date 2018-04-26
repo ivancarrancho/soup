2ba179b37aae4e9efee23528b12bef71300b3d1e
Ivan Ramirez <ivan.ramirez@axiacore.com>
2018-04-26 09:14:32 -0500

    :soccer: comments

--- main.js
+++ main.js
@@ -36,9 +36,7 @@ var words = [
 
 var height = 0;
 var width = 0;
-var allOrientations = ['horizontal','horizontalBack','vertical','verticalUp',
-                       'diagonal','diagonalUp','diagonalBack','diagonalUpBack'];
-
+// Crea la sopa de letras con el tamaño que ingresa el usuario
     function create(height, width) {
         var puzzle = wordfind.newPuzzle(words, {
             height: height,
@@ -48,24 +46,27 @@ var allOrientations = ['horizontal','horizontalBack','vertical','verticalUp',
             preferOverlap: true
         });
 
+    // envío la sopa de letras "puzzle" a la libreria arraytotable para pintar la tabla en el html
         var table = arrayToTable(puzzle, {
             thead: true,
             attrs: {class: 'table'}
         })
-
+        //  se envia al html
         $('.letter-soup').append(table);
     }
 
+    // crea la sopa según la librería wordfind, solamnete recibe las palabras que define arriba
     function create_soup() {
         var gamePuzzle = wordfindgame.create(words, '#puzzle-container', '#puzzle-words');
         $("#solveBTN").click(function(){
             // Solve the puzzle !
             var result = wordfindgame.solve(gamePuzzle, words);
+            // para el tiempo
             parar();
         });
     }
 
-
+    // Toda la Funcion del reloj
     var centesimas = 0;
     var segundos = 0;
     var minutos = 0;
@@ -76,17 +77,7 @@ var allOrientations = ['horizontal','horizontalBack','vertical','verticalUp',
     function parar () {
         clearInterval(control);
     }
-    function reinicio () {
-        clearInterval(control);
-        centesimas = 0;
-        segundos = 0;
-        minutos = 0;
-        horas = 0;
-        Centesimas.innerHTML = ":00";
-        Segundos.innerHTML = ":00";
-        Minutos.innerHTML = ":00";
-        Horas.innerHTML = "00";
-    }
+    //  corre el tiempo
     function cronometro () {
         if (centesimas < 99) {
             centesimas++;
@@ -118,35 +109,48 @@ var allOrientations = ['horizontal','horizontalBack','vertical','verticalUp',
             Horas.innerHTML = horas;
         }
     }
-
+//  Ejecut ale envío del formulario
 $(function() {
     $( "form" ).submit(function( event ) {
         event.preventDefault();
+        //  capturo tamaños
         height =  parseInt($(".form-heigth").val()),
         width =  parseInt($(".form-width").val()),
+        // muestra el bienvenido
         $('.player').show();
+        // muestro nombre y fecha
         $('.player-name').text($(".form-name").val() + ' Fecha de juego:' + new Date());
+        // muestro caja donde irá la sopa
         $('.letter-soup').show();
+        //  arranca el tiempo
         inicio();
+        //  crea la sopa
         create(height, width);
 
     });
 
+    // funcion de creación de la sopa
     $( ".random" ).on('click', function( event ) {
         event.preventDefault();
+        // muestra el bienvenido
         $('.player').show();
+        // captura el nombre si viene
         var name = $(".form-name").val();
+        //  valida si pone el nom,bre o pone anonimo
         if (name) {
             $('.player-name').text($(".form-name").val() + ' Fecha de juego:' + new Date());
         } else {
             $('.player-name').text('Jugador anónimo  Fecha de juego:' + new Date());
         }
-
+        //  muestra la caja donde irá la sopa
         $('.soup').show();
+        //  arranca el tiempo
         inicio();
+        //  crea la sopa
         create_soup();
 
-        $("#puzzle-container button").on('mousedown', function() {
+        //  para el reloj si se completa la sopa
+        $("#puzzle-container button").on('mouseup', function() {
             setTimeout(function(){
                 if ($("#puzzle-container button").first().hasClass( "complete" )) {
                     parar();
